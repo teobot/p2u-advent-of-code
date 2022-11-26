37a4d7fdf18aea15a80588e7bc431cf18c748d0c
Theo Clapperton <theoclapperton@outlook.com>
2022-11-26 01:58:55 +0000

    updated

---
+++ data/2021.js
@@ -0,0 +1,153 @@
+export default data = {
+  "event": "2021",
+  "owner_id": 746545,
+  "closed_at": "2022-01-01T00:00:00Z",
+  "members": {
+    "746545": {
+      "last_star_ts": 1640089238,
+      "id": 746545,
+      "local_score": 139,
+      "global_score": 0,
+      "completion_day_level": {
+        "3": {
+          "2": { "star_index": 495605, "get_star_ts": 1638527969 },
+          "1": { "star_index": 479958, "get_star_ts": 1638523609 }
+        },
+        "5": {
+          "2": { "star_index": 899676, "get_star_ts": 1638719810 },
+          "1": { "get_star_ts": 1638709898, "star_index": 877060 }
+        },
+        "13": {
+          "2": { "get_star_ts": 1639391647, "star_index": 2077290 },
+          "1": { "get_star_ts": 1639391288, "star_index": 2076703 }
+        },
+        "7": {
+          "2": { "get_star_ts": 1638870592, "star_index": 1237323 },
+          "1": { "star_index": 1230873, "get_star_ts": 1638868504 }
+        },
+        "4": {
+          "2": { "star_index": 1145608, "get_star_ts": 1638831627 },
+          "1": { "star_index": 712083, "get_star_ts": 1638625677 }
+        },
+        "10": {
+          "2": { "star_index": 1715631, "get_star_ts": 1639135705 },
+          "1": { "star_index": 1712293, "get_star_ts": 1639133687 }
+        },
+        "11": {
+          "2": { "star_index": 1996734, "get_star_ts": 1639335397 },
+          "1": { "star_index": 1995309, "get_star_ts": 1639334423 }
+        },
+        "6": {
+          "2": { "get_star_ts": 1638793902, "star_index": 1056909 },
+          "1": { "get_star_ts": 1638791702, "star_index": 1051623 }
+        },
+        "8": {
+          "2": { "star_index": 1438434, "get_star_ts": 1638973874 },
+          "1": { "get_star_ts": 1638960624, "star_index": 1414045 }
+        },
+        "2": {
+          "2": { "get_star_ts": 1638438961, "star_index": 237355 },
+          "1": { "star_index": 236005, "get_star_ts": 1638438660 }
+        },
+        "21": { "1": { "star_index": 2743161, "get_star_ts": 1640089238 } },
+        "17": {
+          "2": { "get_star_ts": 1639741629, "star_index": 2476412 },
+          "1": { "star_index": 2474780, "get_star_ts": 1639739871 }
+        },
+        "1": {
+          "2": { "star_index": 964, "get_star_ts": 1638357923 },
+          "1": { "star_index": 0, "get_star_ts": 1638357527 }
+        },
+        "14": { "1": { "get_star_ts": 1639477725, "star_index": 2199757 } },
+        "9": {
+          "2": { "get_star_ts": 1639172372, "star_index": 1773619 },
+          "1": { "star_index": 1563798, "get_star_ts": 1639045291 }
+        }
+      },
+      "stars": 28,
+      "name": "Theo Clapperton"
+    },
+    "1492707": {
+      "name": "Damian Klekot",
+      "stars": 7,
+      "completion_day_level": {
+        "1": {
+          "2": { "star_index": 49673, "get_star_ts": 1638375628 },
+          "1": { "star_index": 21754, "get_star_ts": 1638366192 }
+        },
+        "6": { "1": { "get_star_ts": 1640270692, "star_index": 2858628 } },
+        "3": { "1": { "star_index": 3031619, "get_star_ts": 1640792308 } },
+        "7": { "1": { "get_star_ts": 1639127707, "star_index": 1700357 } },
+        "2": {
+          "1": { "star_index": 281441, "get_star_ts": 1638451951 },
+          "2": { "get_star_ts": 1638453257, "star_index": 285926 }
+        }
+      },
+      "id": 1492707,
+      "local_score": 24,
+      "global_score": 0,
+      "last_star_ts": 1640792308
+    },
+    "1472859": {
+      "completion_day_level": {
+        "1": {
+          "2": { "star_index": 2234066, "get_star_ts": 1639503027 },
+          "1": { "get_star_ts": 1638783204, "star_index": 1029647 }
+        },
+        "3": {
+          "1": { "get_star_ts": 1639943837, "star_index": 2626583 },
+          "2": { "get_star_ts": 1640869061, "star_index": 3046877 }
+        },
+        "2": {
+          "1": { "star_index": 2254921, "get_star_ts": 1639518746 },
+          "2": { "get_star_ts": 1639521219, "star_index": 2257850 }
+        }
+      },
+      "stars": 6,
+      "name": "AlexPullen91",
+      "last_star_ts": 1640869061,
+      "local_score": 14,
+      "global_score": 0,
+      "id": 1472859
+    },
+    "1473338": {
+      "completion_day_level": {
+        "5": {
+          "2": { "star_index": 1311813, "get_star_ts": 1638903794 },
+          "1": { "get_star_ts": 1638902971, "star_index": 1310085 }
+        },
+        "3": {
+          "1": { "get_star_ts": 1638526227, "star_index": 489673 },
+          "2": { "get_star_ts": 1638527893, "star_index": 495360 }
+        },
+        "4": {
+          "1": { "get_star_ts": 1638900391, "star_index": 1304849 },
+          "2": { "star_index": 1306848, "get_star_ts": 1638901392 }
+        },
+        "2": {
+          "2": { "star_index": 240987, "get_star_ts": 1638439814 },
+          "1": { "get_star_ts": 1638439714, "star_index": 240551 }
+        },
+        "1": {
+          "2": { "star_index": 239097, "get_star_ts": 1638439352 },
+          "1": { "get_star_ts": 1638438459, "star_index": 235102 }
+        }
+      },
+      "stars": 10,
+      "name": "Richard-A-Harris",
+      "last_star_ts": 1638903794,
+      "global_score": 0,
+      "local_score": 39,
+      "id": 1473338
+    },
+    "1471975": {
+      "completion_day_level": {},
+      "name": "@smmapletoft",
+      "stars": 0,
+      "last_star_ts": 0,
+      "id": 1471975,
+      "local_score": 0,
+      "global_score": 0
+    }
+  }
+}
